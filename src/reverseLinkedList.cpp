1538f00099aca1e3ac3d4456f19040ee71d57d04
MahikanthNag <MahikanthNag@missionrnd.com>
2016-03-08 23:00:11 +0530

    Submit Commit

--- src/reverseLinkedList.cpp
+++ src/reverseLinkedList.cpp
@@ -1,13 +1,9 @@
 /*
 OVERVIEW: Given a single linked list, reverse the linked list.
 E.g.: 1->2->3->4->5, output is 5->4->3->2->1.
-
 INPUTS: A linked list.
-
 OUTPUT: Reverse the linked list.
-
 ERROR CASES: Return NULL for error cases.
-
 NOTES:
 */
 
@@ -19,5 +15,21 @@ struct node {
 };
 
 struct node * reverseLinkedList(struct node *head) {
-	return NULL;
+
+    if(head == NULL)
+    return NULL;
+    else{
+    struct node* prev   = NULL;
+    struct node* current = head;
+    struct node* next;
+    while (current != NULL)
+    {
+        next  = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+    }
 }
